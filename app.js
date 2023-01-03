8485f0eaeda2064237ac95171910517df88bc349
atha.3417 <atha.3417@gmail.com>
2023-01-03 18:18:16 +0700

    change port for deployment

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
